e21f341a0196a3ec9bac65d2b4390c582f4d5364
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-04-24 16:15:41 +0500

    project complete

--- scrape.py
+++ scrape.py
@@ -36,3 +36,23 @@ product = soup.find('div', attrs= {'class':'XiSiPK AqhsUa'})
 print(product)
 
 
+
+
+
+# from bs4 import BeautifulSoup
+# import requests
+
+# url = "https://www.flipkart.com/canon-eos-3000d-dslr-camera-1-body-18-55-mm-lens/p/itm6f665fea97bc2?pid=CAMF3DHJURPEMNRN&lid=LSTCAMF3DHJURPEMNRNBKP0RG&marketplace=FLIPKART&store=jek%2Fp31&srno=b_1_3&otracker=browse&fm=organic&iid=46c135fc-f5e8-41c4-9a0d-0f5e8914fa81.CAMF3DHJURPEMNRN.SEARCH&ppt=clp&ppn=camera-clp-store&ssid=npop6te04g0000001713950091565"
+
+# response = requests.get(url)
+
+# htmlcontent = response.content
+
+# soup = BeautifulSoup(htmlcontent, 'html.parser')
+
+# titles = []
+# prices = []
+# images = []
+
+# for d in soup.find_all('div', attrs={'class':'_6EBuvT'}):
+#     print(d)
